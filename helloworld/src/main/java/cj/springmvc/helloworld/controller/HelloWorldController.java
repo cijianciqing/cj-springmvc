354591fc9208d1c20fe5bd242a46048eb960f03a
cjcq <425430798@qq.com>
2022-05-10 22:53:10 +0800

    新建module : helloworld

---
+++ helloworld/src/main/java/cj/springmvc/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package cj.springmvc.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/")
+    public String toPage1(){
+        return "page1";
+    }
+}
